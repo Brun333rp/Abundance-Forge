cf1b3d6c5fc037b592a4ce4f71bbb18260e7be1b
Epic312 <kaden_ostendorf@yahoo.com>
2021-01-30 20:05:04 -0700

    lavender but boring

--- src/main/java/com/teamaurora/abundance/common/block/LavenderBlock.java
+++ src/main/java/com/teamaurora/abundance/common/block/LavenderBlock.java
@@ -11,35 +11,29 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 
+import java.util.Random;
 import java.util.function.Supplier;
 
-public class LavenderBlock extends AbnormalsFlowerBlock {
+public class LavenderBlock extends AbnormalsFlowerBlock implements IGrowable {
     public LavenderBlock(Supplier<Effect> stewEffect, int stewEffectDuration, Properties properties) {
         super(stewEffect, stewEffectDuration, properties);
     }
 
-    @Override
-    protected boolean isValidGround(BlockState state, IBlockReader worldIn, BlockPos pos) {
-        return state.getBlock() == AbundanceBlocks.LAVENDER.get() || super.isValidGround(state, worldIn, pos);
+    public boolean canGrow(IBlockReader worldIn, BlockPos pos, BlockState state, boolean isClient) {
+        return true;
     }
 
-    /*@Override
-    public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
-        if (facing == Direction.DOWN && facingState.getBlock() == AbundanceBlocks.LAVENDER.get()) {
-            worldIn.setBlockState(facingPos, AbundanceBlocks.TALL_LAVENDER.get().getDefaultState().with(TallFlowerBlock.HALF, DoubleBlockHalf.LOWER), 2);
-            return AbundanceBlocks.TALL_LAVENDER.get().getDefaultState().with(TallFlowerBlock.HALF, DoubleBlockHalf.UPPER);
-        } else {
-            return super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
-        }
-    }*/
+    public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, BlockState state) {
+        return true;
+    }
 
-    @Override
-    public void neighborChanged(BlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
-        if (worldIn.getBlockState(pos.up()).getBlock() == AbundanceBlocks.LAVENDER.get()) {
-            worldIn.setBlockState(pos, Blocks.AIR.getDefaultState());
-            worldIn.setBlockState(pos.up(), Blocks.AIR.getDefaultState());
-            ((DoublePlantBlock) AbundanceBlocks.TALL_LAVENDER.get()).placeAt(worldIn, pos, 3);
+    public void grow(ServerWorld worldIn, Random rand, BlockPos pos, BlockState state) {
+        DoublePlantBlock doubleplantblock = (DoublePlantBlock)(AbundanceBlocks.TALL_LAVENDER.get());
+        if (doubleplantblock.getDefaultState().isValidPosition(worldIn, pos) && worldIn.getBlockState(pos.up()).getBlock() == AbundanceBlocks.LAVENDER.get()) {
+            doubleplantblock.placeAt(worldIn, pos, 2);
         }
+
     }
 }
