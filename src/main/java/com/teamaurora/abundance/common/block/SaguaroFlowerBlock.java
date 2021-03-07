90019daf1cb8f825331dfc6fcd8211266a20957c
Epic312 <kaden_ostendorf@yahoo.com>
2021-03-07 13:47:53 -0700

    fixed #11

--- src/main/java/com/teamaurora/abundance/common/block/SaguaroFlowerBlock.java
+++ src/main/java/com/teamaurora/abundance/common/block/SaguaroFlowerBlock.java
@@ -7,16 +7,26 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.BushBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorldReader;
 import net.minecraftforge.common.IPlantable;
 import net.minecraftforge.common.PlantType;
 
 public class SaguaroFlowerBlock extends BushBlock {
+    protected static final VoxelShape SHAPE = Block.makeCuboidShape(5.0D, 0.0D, 5.0D, 11.0D, 10.0D, 11.0D);
+
     public SaguaroFlowerBlock(Properties properties) {
         super(properties);
     }
 
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPE;
+    }
+
+    @Override
     public ItemStack getItem(IBlockReader worldIn, BlockPos pos, BlockState state) {
         return new ItemStack(AbundanceItems.SAGUARO_FLOWER.get());
     }
