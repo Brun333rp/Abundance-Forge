ed7f1de565f8dbfe47520a79f412705f43eada64
Sarinsa <mikkemusbruker@gmail.com>
2021-05-12 23:54:12 +0200

    Removed unused mixin helper field (mixin is very uncool :pensive:)

--- src/main/java/com/teamaurora/abundance/common/misc/mixinwork/MixinClientHooks.java
+++ src/main/java/com/teamaurora/abundance/common/misc/mixinwork/MixinClientHooks.java
@@ -1,19 +1,11 @@
 package com.teamaurora.abundance.common.misc.mixinwork;
 
 import com.teamaurora.abundance.common.capability.CapabilityHelper;
-import com.teamaurora.abundance.core.Abundance;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.audio.IAmbientSoundHandler;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-
 public class MixinClientHooks {
-
-    @SuppressWarnings("all")
-    private static final Iterator<IAmbientSoundHandler> EMPTY_SOUND_HANDLER_ITERATOR = new ArrayList<IAmbientSoundHandler>().iterator();
 
     /**
      * Checks if the client player is deafened and
